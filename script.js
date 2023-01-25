9c3861ef96197e5a5bdd4313c5e22a44873771d4
ganesh <ganeshparjaptm5@gmail.com>
2023-01-25 21:10:14 +0530

    modify js file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Welcome to tic tac toe")
+let music = new Audio("music.mp3")
+let turn = new  Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+//function to change tohe turn
+const changeTurn =()=>{
+    return === "x"?"0":"x"
+
+}
+
+
+}
+//game logic statrt
